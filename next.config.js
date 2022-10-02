0a7ef400c4651f69ae4f1a789ce5d31df111b542
j0hannr <johann.rohn@gmail.com>
2022-10-02 15:40:05 +0200

    💡 Mail update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+module.exports = {
+  swcMinify: true, // error when enabled! Rust compiler enabled (faster)
+  reactStrictMode: true, // highlighting potential problems in an application
+  images: { // allow external resources for images
+    domains: ["api.mapbox.com","www.wuppertal.de"],
+  },
+  webpack(config) { // webpack config
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
--- pages/api/complex-mail.js
+++ pages/api/complex-mail.js
@@ -1,4 +1,15 @@
-import * as mjmlReact from "@faire/mjml-react";
+import {
+    render,
+    Mjml,
+    MjmlHead,
+    MjmlTitle,
+    MjmlPreview,
+    MjmlBody,
+    MjmlSection,
+    MjmlColumn,
+    MjmlButton,
+    MjmlImage,
+  } from "@faire/mjml-react";
 
 export default async function handler(req, res) {
   // Require:
@@ -24,98 +35,32 @@ export default async function handler(req, res) {
 }
 
 function MailHtml(host) {
-  const { html, errors } = mjmlReact.render(
-    <mjmlReact.Mjml>
-      <mjmlReact.MjmlHead>
-        <mjmlReact.MjmlFont
-          name="Barlow"
-          href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;700;800&display=swap"
-        />
-        <mjmlReact.MjmlAttributes>
-          <mjmlReact.MjmlButton inner-padding="0" />
-          <mjmlReact.MjmlStyle>
-            {`
-                  .link-nostyle { color: inherit; text-decoration: none }; 
-                  .apple-link-black a,
-                  .apple-link-black,
-                  .apple-link-black div a  { color:#000 !important; text-decoration:none; } 
-                  .apple-link-blue a,
-                  .apple-link-blue,
-                  .apple-link-blue div a  { color:#3292EA !important; text-decoration:none; } 
-                  .btn-link a:hover { background-color: #2B6FAE; } 
-                  .hover:hover td, .hover:hover p { background-color: #2B6FAE !important; }
-                `}
-          </mjmlReact.MjmlStyle>
-        </mjmlReact.MjmlAttributes>
-      </mjmlReact.MjmlHead>
-      <mjmlReact.MjmlBody background-color="#EBEFFB">
-        <mjmlReact.MjmlWrapper padding="40px 15px">
-          <mjmlReact.MjmlSection background-color="#fff" border-radius="25px">
-            <mjmlReact.MjmlColumn>
-              <mjmlReact.MjmlText
-                css-class="apple-link-black"
-                font-size="20px"
-                font-family="Barlow"
-                font-weight="800"
-              >
-                <a href="#">
-                  energie<span style={{ color: "#3292EA" }}>quartier.io</span>
-                </a>
-              </mjmlReact.MjmlText>
-              <mjmlReact.MjmlText
-                font-size="40px"
-                font-weight="800"
-                font-family="Barlow"
-              >
-                Willkommen im energiequartier.
-              </mjmlReact.MjmlText>
-              <mjmlReact.MjmlButton
-                align="left"
-                font-size="18px"
-                padding="20px"
-                inner-padding="16px 20px"
-                border-radius="15px"
-                font-weight="700"
-                font-family="Barlow"
-                background-color="#3292EA"
-                title="Zum Dashboard"
-                css-class="hover"
-                // href={url}
-              >
-                Zum Dashboard
-              </mjmlReact.MjmlButton>
-              <mjmlReact.MjmlImage
-                src={`${host}/images/mail/immobilie-registrieren.png`}
-              />
-              <mjmlReact.MjmlImage
-                src={`${host}/images/mail/öffentliche-daten-finden.png`}
-              />
-              <mjmlReact.MjmlImage
-                src={`${host}/images/mail/gebäudeanalyse-freischalten.png`}
-              />
-            </mjmlReact.MjmlColumn>
-          </mjmlReact.MjmlSection>
-          <mjmlReact.MjmlSection
-            full-width="full-width"
-            text-align="left"
-            padding="15px 0px 0px 0px"
-          >
-            <mjmlReact.MjmlColumn padding="0px">
-              <mjmlReact.MjmlText
-                align="left"
-                font-size="15px"
-                font-weight="500"
-                font-family="Barlow"
-              >
-                Wenn Du diese E-Mail nicht angefordert hast, kannst du sie
-                einfach ignorieren.
-              </mjmlReact.MjmlText>
-            </mjmlReact.MjmlColumn>
-          </mjmlReact.MjmlSection>
-        </mjmlReact.MjmlWrapper>
-      </mjmlReact.MjmlBody>
-    </mjmlReact.Mjml>,
-    { validationLevel: "soft" }
-  );
+    const { html, errors } = render(
+        <Mjml>
+          <MjmlHead>
+            <MjmlTitle>Last Minute Offer</MjmlTitle>
+            <MjmlPreview>Last Minute Offer...</MjmlPreview>
+          </MjmlHead>
+          <MjmlBody width={500}>
+            <MjmlSection fullWidth backgroundColor="#efefef">
+              <MjmlColumn>
+                <MjmlImage src="https://static.wixstatic.com/media/5cb24728abef45dabebe7edc1d97ddd2.jpg" />
+              </MjmlColumn>
+            </MjmlSection>
+            <MjmlSection>
+              <MjmlColumn>
+                <MjmlButton
+                  padding="20px"
+                  backgroundColor="#346DB7"
+                  href="https://www.wix.com/"
+                >
+                  I like it!
+                </MjmlButton>
+              </MjmlColumn>
+            </MjmlSection>
+          </MjmlBody>
+        </Mjml>,
+        { validationLevel: "soft" }
+      );
   return html;
 }
